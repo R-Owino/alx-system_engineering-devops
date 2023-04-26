5409c5e978a49e28a0c1357ccdfa59d8989042ce
R-Owino <sayeakinyi@gmail.com>
2023-04-26 16:38:09 +0000

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Task 0 extended:
+    - Export data in CSV format
+"""
 
 import csv
 import requests
@@ -14,11 +18,13 @@ if __name__ == "__main__":
     # Retrieve the user's username from the response JSON
     username = user_request.get("username")
     # Send a GET request to retrieve all the todos for the user
-    todos_request = requests.get(api_url + "todos", params={"userId": user_id}).json()
+    todos_request = requests.get(api_url + "todos",
+                                 params={"userId": user_id}).json()
     # Open a new CSV file for writing
     with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         # Create a new CSV writer object
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         # Write each todo's information to the CSV file
         for todo in todos_request:
-            writer.writerow([user_id, username, todo.get("completed"), todo.get("title")])
+            writer.writerow([user_id, username,
+                             todo.get("completed"), todo.get("title")])
