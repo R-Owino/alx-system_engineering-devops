2a0d8df87fc69fe58297095bb819e95a4a3245e9
R-Owino <sayeakinyi@gmail.com>
2023-04-26 04:08:31 +0000

    Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    # Retrieve the user ID from the command-line arguments
+    user_id = sys.argv[1]
+    # Define the base URL for the REST API
+    api_url = "https://jsonplaceholder.typicode.com/"
+    # Send a GET request to retrieve the user's information
+    user_request = requests.get(api_url + "users/{}".format(user_id)).json()
+    # Retrieve the user's username from the response JSON
+    username = user_request.get("username")
+    # Send a GET request to retrieve all the todos for the user
+    todos_request = requests.get(api_url + "todos", params={"userId": user_id}).json()
+    # Open a new CSV file for writing
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        # Create a new CSV writer object
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        # Write each todo's information to the CSV file
+        for todo in todos_request:
+            writer.writerow([user_id, username, todo.get("completed"), todo.get("title")])
