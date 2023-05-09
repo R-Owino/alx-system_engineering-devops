0bfcc8e061418f7b1700927cc077f15cc4744dd0
R-Owino <sayeakinyi@gmail.com>
2023-05-09 21:26:41 +0000

    Number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "My User Agent"}
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            subscribers = data["data"]["subscribers"]
+            return subscribers
+        else:
+            return 0
+    except requests.exceptions.RequestException:
+        return 0
