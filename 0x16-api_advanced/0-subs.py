fb7c91505c1d37f99e6fd0b4d0ac468938bb6367
R-Owino <sayeakinyi@gmail.com>
2023-05-09 21:47:16 +0000

    Number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,6 +6,7 @@ queries the Reddit API and returns the number of subscribers
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "My User Agent"}
